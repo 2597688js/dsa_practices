ee984545553c882d56b732449ff00abeca1711af
Janarddan Sarkar <janarddansarkar@gmail.com>
2024-02-28 17:13:46 +0530

    pushed on 28-02-2024

--- recursion/3.print_N_to_1.py
+++ recursion/3.print_N_to_1.py
@@ -6,13 +6,13 @@ Description : print from N to 1 using recursion
 """
 
 
-def print_N_1(i,n):
-    if i < 1:
+def print_N_1(n):
+    if n < 1:
         return
 
-    print(i)
-    print_N_1(i-1, n)
+    print(n)
+    print_N_1(n-1)
 
 
 if __name__ == "__main__":
-    print_N_1(i=3, n=3)
+    print_N_1(n=3)
