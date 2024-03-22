611d5a4f486c4b96170ebfe5a030172ee31c1811
Janarddan Sarkar <janarddansarkar@gmail.com>
2024-03-22 17:02:52 +0530

    pushed on 22-03-2024

--- bit_manipulation/10.clear_ith_bit.py
+++ bit_manipulation/10.clear_ith_bit.py
@@ -13,10 +13,11 @@ Logic:
     perform  AND operation of N with negation of ( 1 << i) i.e., N & ~(1 << i). It will clear the i-th bit
 """
 def clear_ith_bit_optimal(decimal_num, i):
-    return decimal_num | ~(1 << i)
+    return decimal_num & ~(1 << i)
 
 
 if __name__ == "__main__":
     print("Optimal : ", clear_ith_bit_optimal(9, 1))
     print("Optimal : ", clear_ith_bit_optimal(13, 2))
     print("Optimal : ", clear_ith_bit_optimal(7, 1))
+    print("Optimal : ", clear_ith_bit_optimal(5, 0))
