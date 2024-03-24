df0d83118f259613bce59c0d9c66b077a8f5e836
2597688js <janarddansarkarxyz@gmail.com>
2024-03-24 17:15:09 +0530

    pushed on 24-03-2024 from PC

---
+++ searching/1.linear_search.py
@@ -0,0 +1,6 @@
+"""
+Author : Janarddan Sarkar
+file_name : 1.linear_search.py 
+date : 24-03-2024
+description : 
+"""
---
+++ searching/2.binary_search.py
@@ -0,0 +1,6 @@
+"""
+Author : Janarddan Sarkar
+file_name : 2.binary_search.py 
+date : 24-03-2024
+description : 
+"""
