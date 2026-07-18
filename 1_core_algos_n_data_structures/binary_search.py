5e0679fb7bc6bf1f105a8147ae0fb9e7366d4e00
Janarddan Sarkar <janarddansarkar@gmail.com>
2026-07-19 02:30:45 +0530

    binary search practiced

---
+++ 1_core_algos_n_data_structures/binary_search.py
@@ -0,0 +1,53 @@
+"""
+Author : janarddan
+File_name = binary_search.py
+Date : 19/07/26
+Description : Implementation of Binary Search Algorithm
+
+"""
+
+def binary_search(arr, target):
+    n = len(arr)
+    left = 0
+    right = n-1
+
+    while left <= right:
+        mid = left + (right - left)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+def binary_search1(arr, target):
+    n = len(arr)
+    left = 0
+    right = n-1
+
+    while left <= right:
+        mid = left + (right - left)//2
+
+        if arr[mid] < target:
+            left = mid + 1
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            return mid
+
+    return -1
+
+
+
+if __name__ == '__main__':
+    arr = [1,2,3,4,5,6,7,8,9,10]
+    print(binary_search(arr, 23))
+    print(binary_search([2,5,8,12,16,23,38,56,72], 23))
+    print(binary_search([2,5,8,12,16,23,38,56,72], 16))
+    print()
+    print(binary_search1(arr, 23))
+    print(binary_search1([2, 5, 8, 12, 16, 23, 38, 56, 72], 23))
+    print(binary_search1([2, 5, 8, 12, 16, 23, 38, 56, 72], 16))
