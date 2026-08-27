45aa9460d35a68bd7153ea8f0231a22d076e4b20
Janarddan Sarkar <janarddansarkar@gmail.com>
2026-08-27 14:42:08 +0530

    find pivot index - prefix sum

---
+++ 06_prefix_sum/4.Find Pivot Index.py
@@ -0,0 +1,101 @@
+"""
+Author : janarddan
+File_name = 4.Find Pivot Index.py
+Date : 27/08/26
+Description :
+
+Leetcode 724. Find Pivot Index (Easy)
+
+## Given an array of integers nums, calculate the pivot index of this array.
+ - The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum
+  of all the numbers strictly to the index's right.
+
+ - If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left.
+  This also applies to the right edge of the array.
+
+ - Return the leftmost pivot index. If no such index exists, return -1.
+
+
+** Example 1: **
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+
+** Example 2: **
+
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+
+** Example 3: **
+
+Input: nums = [2,1,-1]
+Output: 0
+Explanation:
+The pivot index is 0.
+Left sum = 0 (no elements to the left of index 0)
+Right sum = nums[1] + nums[2] = 1 + -1 = 0
+
+"""
+
+class Solution:
+    def pivotIndexBruteforce(self, nums: list[int]) -> int:
+        """
+        TC -> O(n**2)
+        SC -> O(1)
+        """
+        n = len(nums)
+
+        for i in range(n):
+            leftSum, rightSum = 0, 0
+
+            for j in range(0, i):
+                leftSum += nums[j]
+
+            for k in range(i+1, n):
+                rightSum += nums[k]
+
+            if leftSum == rightSum:
+                return i
+                # return nums[i]
+
+        return -1
+
+    def pivotIndexOptimal(self, nums: list[int]) -> int:
+        """
+        TC -> O(n)
+        SC -> O(1)
+        """
+        n = len(nums)
+
+        leftSum = 0
+
+        totalSum = 0
+        for i in range(n):
+            totalSum += nums[i]
+
+        for j in range(n):
+            rightSum = totalSum - leftSum - nums[j]
+            if leftSum == rightSum:
+                return j
+
+            leftSum += nums[j]
+
+        return -1
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.pivotIndexBruteforce([1,7,3,6,5,6]))
+    print(sol.pivotIndexBruteforce([1,2,3]))
+    print(sol.pivotIndexBruteforce([2,1,-1]))
+    print()
+    print(sol.pivotIndexOptimal([1,7,3,6,5,6]))
+    print(sol.pivotIndexOptimal([1,2,3]))
+    print(sol.pivotIndexOptimal([2,1,-1]))
+
